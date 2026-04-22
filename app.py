63b5076ed586b728b04bba59535ea9c1d734521d
Janet <janetsara05@gmail.com>
2026-04-22 16:24:32 +0530

    Added Day 1 log

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+tasks = ["Learn DevOps", "Build Project"]
+
 @app.route('/')
 def home():
     return "Hello, DevOps Journey!"
+
+@app.route('/tasks')
+def get_tasks():
+    return {"tasks": tasks}
 
 if __name__ == '__main__':
     app.run(debug=True)
